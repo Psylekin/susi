8fa2597d4715ec3a3b98f13016510743f5349d23
Ben Lenk-Ostendorf <ben.lenkostendorf@gmail.com>
2018-05-31 13:11:51 +0200

    Erster Versuch für Susi's Seite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RiddlesComponent } from './riddles/riddles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RiddlesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/riddles/riddles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-riddles',
+  templateUrl: './riddles.component.html',
+  styleUrls: ['./riddles.component.css']
+})
+export class RiddlesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
