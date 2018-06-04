7d3211c264c1b3bf229e7d7d2363dbea8b6b292d
Ben Lenk-Ostendorf <ben.lenkostendorf@gmail.com>
2018-06-05 00:39:01 +0200

    first commit

--- src/app/riddles/poetic-riddle/poetic-riddle.component.ts
+++ src/app/riddles/poetic-riddle/poetic-riddle.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-poetic-riddle',
   templateUrl: './poetic-riddle.component.html',
-  styles: []
+  styles: ['p{text-align: center;}']
 })
 export class PoeticRiddleComponent implements OnInit {
 
