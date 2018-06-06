8416fcd284e03dae1aa3ade6150cdaa3e53a6519
Ben Lenk-Ostendorf <lebe63@ads.medisinn.net>
2018-06-06 18:24:27 +0200

    Footer raus und Anpassungen

--- src/app/riddles/riddles.component.ts
+++ src/app/riddles/riddles.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 })
 export class RiddlesComponent implements OnInit {
 
+  switch = false;
 
   constructor() {
   }
--- src/app/riddles/solution-riddle/solution-riddle.component.ts
+++ src/app/riddles/solution-riddle/solution-riddle.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolutionRiddleComponent implements OnInit {
 
+  switch = false;
+
   constructor() { }
 
   ngOnInit() {
